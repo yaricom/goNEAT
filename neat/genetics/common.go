6d5c853f07d6c71ec9ec9a562f28e880731aee3f
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-09-11 18:51:37 +0300

    Added genetics stubs

---
+++ neat/genetics/common.go
@@ -0,0 +1,5 @@
+// Package genetics holds data holders and helper utilities used to implement genetic evolution algorithm
+package genetics
+
+
+
---
+++ neat/genetics/genome.go
@@ -0,0 +1,5 @@
+package genetics
+
+type Genome interface {
+
+}
