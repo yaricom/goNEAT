2257b59c9a85138d70b836f939d19e1da253a564
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-09-13 19:13:27 +0300

    Started NNode tests implementation

---
+++ neat_test/network_test/nnode_test.go
@@ -0,0 +1,32 @@
+package network_test
+
+import (
+	"testing"
+	"fmt"
+	"strings"
+	"github.com/yaricom/goNEAT/neat/network"
+)
+
+// Tests how NNode read working
+func TestReadNNode(t *testing.T) {
+	node_id, trait_id, ntype, gen_node_label := 1, 10, network.SENSOR, network.INPUT
+	node_str := fmt.Sprintf("%d %d %d %d", node_id, trait_id, ntype, gen_node_label)
+
+	trait := network.NewTraitWithParams(10, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
+	traits := []network.Trait{trait}
+
+	node := network.ReadNNode(strings.NewReader(node_str), traits)
+
+	if node.NodeId() != node_id {
+		t.Errorf("Found node ID is not what expected, %d != %d", node_id, node.NodeId())
+	}
+	if node.GetTrait() != trait {
+		t.Error("The wrong Trait found in the node")
+	}
+	if node.GetType() != ntype {
+		t.Errorf("Wrong node type found, %d != %d", ntype, node.GetFtype())
+	}
+	if node.GenNodeLabel() != gen_node_label {
+		t.Errorf("The wrong node placement label found, %d != %d", gen_node_label, node.GenNodeLabel())
+	}
+}
