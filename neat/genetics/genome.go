b10777a4ba8de9e2fa97769ebef1ab66edaa87ac
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-09-15 13:25:19 +0300

    Updated Genome implementation to include new field and method

--- neat/genetics/genome.go
+++ neat/genetics/genome.go
@@ -1,5 +1,26 @@
 package genetics
 
-type Genome interface {
+import "github.com/yaricom/goNEAT/neat/network"
 
+// A Genome is the primary source of genotype information used to create  a phenotype.
+// It contains 3 major constituents:
+// 	1) A Vector of Traits
+// 	2) A List of NNodes pointing to a Trait from (1)
+// 	3) A List of Genes with Links that point to Traits from (1)
+//
+// (1) Reserved parameter space for future use.
+// (2) NNode specifications.
+// (3) Is the primary source of innovation in the evolutionary Genome.
+//
+// Each Gene in (3) has a marker telling when it arose historically. Thus, these Genes can be used to speciate the
+// population, and the list of Genes provide an evolutionary history of innovation and link-building.
+type Genome struct {
+	GenomeId int
+
+
+}
+
+// Generate a Network phenotype from this Genome with specified id
+func (g *Genome) Genesis(net_id int) *network.Network {
+	return nil
 }
