d551a1656fff8ae713d558b3ac2c0c14f2848738
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-09-15 14:51:24 +0300

    Fixed fields names of configuration holder

--- neat/neat.go
+++ neat/neat.go
@@ -11,75 +11,75 @@ const Num_trait_params = 8
 // The global configuration holder
 type Neat struct {
 	// Prob. of mutating a single trait param
-	trait_param_mut_prob float64
+	TraitParamMutProb      float64
 	// Power of mutation on a single trait param
-	trait_mutation_power float64
+	TraitMutationPower     float64
 	// Amount that mutation_num changes for a trait change inside a link
-	linktrait_mut_sig float64
+	LinkTraitMutSig        float64
 	// Amount a mutation_num changes on a link connecting a node that changed its trait
-	nodetrait_mut_sig float64
+	NodeTraitMutSig        float64
 	// The power of a linkweight mutation
-	weight_mut_power float64
+	WeightMutPower         float64
 
 	// These 3 global coefficients are used to determine the formula for
-     	// computing the compatibility between 2 genomes.  The formula is:
-     	// disjoint_coeff * pdg + excess_coeff * peg + mutdiff_coeff * mdmg.
-     	// See the compatibility method in the Genome class for more info
-     	// They can be thought of as the importance of disjoint Genes,
-     	// excess Genes, and parametric difference between Genes of the
-     	// same function, respectively.
-	disjoint_coeff float64
-	excess_coeff float64
-	mutdiff_coeff float64
+	// computing the compatibility between 2 genomes.  The formula is:
+	// disjoint_coeff * pdg + excess_coeff * peg + mutdiff_coeff * mdmg.
+	// See the compatibility method in the Genome class for more info
+	// They can be thought of as the importance of disjoint Genes,
+	// excess Genes, and parametric difference between Genes of the
+	// same function, respectively.
+	DisjointCoeff          float64
+	ExcessCoeff            float64
+	MutdiffCoeff           float64
 
 	// This global tells compatibility threshold under which
-     	// two Genomes are considered the same species */
-	compat_threshold float64
+	// two Genomes are considered the same species */
+	CompatThreshold        float64
 
 	/* Globals involved in the epoch cycle - mating, reproduction, etc.. */
 
 	// How much does age matter?
-	age_significance float64
+	AgeSignificance        float64
 	// Percent of ave fitness for survival
-	survival_thresh float64
+	SurvivalThresh         float64
 
 	// Probabilities of a non-mating reproduction
-	mutate_only_prob float64
-	mutate_random_trait_prob float64
-	mutate_link_trait_prob float64
-	mutate_node_trait_prob float64
-	mutate_link_weights_prob float64
-	mutate_toggle_enable_prob float64
-	mutate_gene_reenable_prob float64
-	mutate_add_node_prob float64
-	mutate_add_link_prob float64
+	MutateOnlyProb         float64
+	MutateRandomTraitProb  float64
+	MutateLinkTraitProb    float64
+	MutateNodeTraitProb    float64
+	MutateLinkWeightsProb  float64
+	MutateToggleEnableProb float64
+	MutateGeneReenableProb float64
+	MutateAddNodeProb      float64
+	MutateAddLinkProb      float64
 
 	// Probabilities of a mate being outside species
-	interspecies_mate_rate float64
-	mate_multipoint_prob float64
-	mate_multipoint_avg_prob float64
-	mate_singlepoint_prob float64
+	InterspeciesMateRate   float64
+	MateMultipointProb     float64
+	MateMultipointAvgProb  float64
+	MateSinglepointProb    float64
 
 	// Prob. of mating without mutation
-	mate_only_prob float64
+	MateOnlyProb           float64
 	// Probability of forcing selection of ONLY links that are naturally recurrent
-	recur_only_prob float64
+	RecurOnlyProb          float64
 
 	// Size of population
-	pop_size int32
+	PopSize                int32
 	// Age where Species starts to be penalized
-	dropoff_age int32
+	DropOffAge             int32
 	// Number of tries mutate_add_link will attempt to find an open link
-	newlink_tries int32
+	NewLinkTries           int32
 
 	// Tells to print population to file every n generations
-	print_every int32
+	PrintEvery             int32
 
 	// The number of babies to siphen off to the champions
-	babies_stolen int32
+	BabiesStolen           int32
 
 	// The number of runs to average over in an experiment
-	num_runs int32
+	Num_runs               int32
 }
 
 // Returns
