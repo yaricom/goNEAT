db62ee8d0486c67ffab773a6e0b76e00e2119bfa
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-09-15 16:04:10 +0300

    Added sort by fitness and to string

--- neat/genetics/organism.go
+++ neat/genetics/organism.go
@@ -1,57 +1,60 @@
 package genetics
 
-import "github.com/yaricom/goNEAT/neat/network"
+import (
+	"github.com/yaricom/goNEAT/neat/network"
+	"fmt"
+)
 
 // Organisms are Genomes and Networks with fitness information,
 // i.e. the genotype and phenotype together.
 type Organism struct {
 	// A measure of fitness for the Organism
-	Fitness float64
+	Fitness                   float64
 	// A fitness measure that won't change during adjustments
-	OriginalFitness float64
+	OriginalFitness           float64
 
 	// Win marker (if needed for a particular task)
-	IsWinner bool
+	IsWinner                  bool
 
 	// The Organism's phenotype
-	Net *network.Network
+	Net                       *network.Network
 	// The Organism's genotype
-	GNome *Genome
+	GNome                     *Genome
 	// The Species of the Organism
-	SpeciesOf *Species
+	SpeciesOf                 *Species
 
 	// Number of children this Organism may have
-	ExpectedOffspring int64
+	ExpectedOffspring         int64
 	// Tells which generation this Organism is from
-	Generation int32
+	Generation                int32
 
 	// Marker for destruction of inferior Organisms
-	ToEliminate bool
+	ToEliminate               bool
 	// Marks the species champion
-	IsChampion bool
+	IsChampion                bool
 
 	// Number of reserved offspring for a population leader
-	SuperChampOffspring int
+	SuperChampOffspring       int
 	// Marks the best in population
-	IsPopulationChampion bool
+	IsPopulationChampion      bool
 	// Marks the duplicate child of a champion (for tracking purposes)
 	IsPopulationChampionChild bool
 
 	// DEBUG variable - highest fitness of champ
-	HighestFitness float64
+	HighestFitness            float64
 
 	// Track its origin - for debugging or analysis - we can tell how the organism was born
-	MutationStructBaby bool
-	MateBaby bool
+	MutationStructBaby        bool
+	MateBaby                  bool
 }
 
 // Creates new organism with specified genome, fitness and given generation number
-func NewOrganism(fit float64, g *Genome, gen int32) *Organism {
+func NewOrganism(fit float64, g *Genome, generation int32) *Organism {
 	return &Organism{
 		Fitness:fit,
 		GNome:g,
 		Net:g.Genesis(g.GenomeId),
-		Generation:gen,
+		Generation:generation,
 	}
 }
 
@@ -62,4 +65,20 @@ func (o *Organism) UpdatePhenotype() {
 
 	// Now, recreate the phenotype off the new genotype
 	o.Net = o.GNome.Genesis(o.GNome.GenomeId)
+}
+
+func (o *Organism) String() string {
+	return fmt.Sprintf("[Organism, fitness: %f]", o.Fitness)
+}
+
+// ByFitness implements sort.Interface for []Organism based on the Fitness field.
+type ByFitness []*Organism
+func (f ByFitness) Len() int {
+	return len(f)
+}
+func (f ByFitness) Swap(i, j int) {
+	f[i], f[j] = f[j], f[i]
+}
+func (f ByFitness) Less(i, j int) bool {
+	return f[i].Fitness < f[j].Fitness
 }
