46012a1e935e28e1781006baa5b2527394da9ca5
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-09-18 19:12:21 +0300

    Added constant

--- neat/genetics/common.go
+++ neat/genetics/common.go
@@ -1,6 +1,11 @@
 // Package genetics holds data holders and helper utilities used to implement genetic evolution algorithm
 package genetics
 
+import (
+	"github.com/leesper/go_rng"
+	"time"
+)
+
 // The innovation method to be applied
 const (
 	// The novelty will be introduced by new NN node
@@ -9,3 +14,13 @@ const (
 	NEWLINK
 )
 
+// Mutators are variables that specify a kind of mutation of connection weights between NN nodes
+const (
+	//This adds Gaussian noise to the weights
+	GAUSSIAN = iota
+	//This sets weights to numbers chosen from a Gaussian distribution
+	COLDGAUSSIAN
+)
+
+// The Gaussian random number generator
+const gaussian = rng.NewGaussianGenerator(time.Now().UnixNano())
