34dbd826d6d67759145b6481dc31943f703f153c
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-09-21 18:02:29 +0300

    Implemented additional stub methods

--- neat/genetics/population.go
+++ neat/genetics/population.go
@@ -2,15 +2,34 @@ package genetics
 
 type Population struct {
 	// Species in the Population. Note that the species should comprise all the genomes
-	Species     []*Species
+	Species            []*Species
 	// The highest species number
-	LastSpecies int
+	LastSpecies        int
 	// For holding the genetic innovations of the newest generation
-	Innovations []*Innovation
+	Innovations        []*Innovation
+	// An integer that when above zero tells when the first winner appeared
+	WinnerGen          int
+
+
+	// Stagnation detector
+	HighestFitness     float64
+	// If too high, leads to delta coding
+	HighestLastChanged int
 
 
 	// The current innovation number for population
-	currInnovNum int64
+	currInnovNum       int64
+}
+
+/* Construct off of a single spawning Genome */
+func NewPopulation(g *Genome, size int) *Population {
+	pop := Population{
+		WinnerGen:0,
+		HighestFitness:0.0,
+		HighestLastChanged:0,
+	}
+	pop.spawn(g, size)
+	return &pop
 }
 
 // Returns current innovation number and increment innovations number counter after that
@@ -18,4 +37,11 @@ func (p *Population) getInnovationNumberAndIncrement() int64 {
 	inn_num := p.currInnovNum
 	p.currInnovNum++
 	return inn_num
+}
+
+// A Population can be spawned off of a single Genome. There will be size Genomes added to the Population.
+// The Population does not have to be empty to add Genomes.
+func (p *Population) spawn(g *Genome, size int) bool {
+	// TODO implement this
+	return false
 }
