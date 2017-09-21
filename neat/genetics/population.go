b8bdbcfcdb12611104581016631a55c3346c7c70
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-09-21 16:43:22 +0300

    Added additional fields

--- neat/genetics/population.go
+++ neat/genetics/population.go
@@ -2,8 +2,20 @@ package genetics
 
 type Population struct {
 	// Species in the Population. Note that the species should comprise all the genomes
-	species []*Species
+	Species     []*Species
 	// The highest species number
-	lastSpecies int
+	LastSpecies int
+	// For holding the genetic innovations of the newest generation
+	Innovations []*Innovation
 
+
+	// The current innovation number for population
+	currInnovNum int64
+}
+
+// Returns current innovation number and increment innovations number counter after that
+func (p *Population) getInnovationNumberAndIncrement() int64 {
+	inn_num := p.currInnovNum
+	p.currInnovNum++
+	return inn_num
 }
