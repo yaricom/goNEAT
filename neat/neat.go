0cfd272209376dcc09e1b335d36c9adfbbec95a2
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-10-03 17:25:08 +0300

    Implemented load Neat context configuration and corresponding test case

--- neat/neat.go
+++ neat/neat.go
@@ -5,6 +5,7 @@ package neat
 import (
 	"math/rand"
 	"fmt"
+	"io"
 )
 
 // The number of parameters used in neurons that learn through habituation,
@@ -82,10 +83,88 @@ type NeatContext struct {
 	BabiesStolen           int
 
 	// The number of runs to average over in an experiment
-	Num_runs               int
+	NumRuns                int
 
 	// The flag to indicate whether to print additional debugging info
 	IsDebugEnabled         bool
+
+	/* OBSOLETE */
+	recurProb float64
+}
+
+// Loads context configuration from provided reader
+func LoadContext(r io.Reader) *NeatContext {
+	c := NeatContext{}
+	// read configuration
+	var name string
+	// trait_param_mut_prob 0.5
+	fmt.Fscanf(r, "%s %f ", &name, &c.TraitParamMutProb)
+	// trait_mutation_power 1.0
+	fmt.Fscanf(r, "%s %f ", &name, &c.TraitMutationPower)
+	// linktrait_mut_sig 1.0
+	fmt.Fscanf(r, "%s %f ", &name, &c.LinkTraitMutSig)
+	// nodetrait_mut_sig 0.5
+	fmt.Fscanf(r, "%s %f ", &name, &c.NodeTraitMutSig)
+	// weight_mut_power 1.8
+	fmt.Fscanf(r, "%s %f ", &name, &c.WeightMutPower)
+	// recur_prob 0.05
+	fmt.Fscanf(r, "%s %f ", &name, &c.recurProb)
+	// disjoint_coeff 1.0
+	fmt.Fscanf(r, "%s %f ", &name, &c.DisjointCoeff)
+	// excess_coeff 1.0
+	fmt.Fscanf(r, "%s %f ", &name, &c.ExcessCoeff)
+	// mutdiff_coeff 3.0
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutdiffCoeff)
+	// compat_thresh 4.0
+	fmt.Fscanf(r, "%s %f ", &name, &c.CompatThreshold)
+	// age_significance 1.0
+	fmt.Fscanf(r, "%s %f ", &name, &c.AgeSignificance)
+	// survival_thresh 0.4
+	fmt.Fscanf(r, "%s %f ", &name, &c.SurvivalThresh)
+	// mutate_only_prob 0.25
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateOnlyProb)
+	// mutate_random_trait_prob 0.1
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateRandomTraitProb)
+	// mutate_link_trait_prob 0.1
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateLinkTraitProb)
+	// mutate_node_trait_prob 0.1
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateNodeTraitProb)
+	// mutate_link_weights_prob 0.8
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateLinkWeightsProb)
+	// mutate_toggle_enable_prob 0.1
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateToggleEnableProb)
+	// mutate_gene_reenable_prob 0.05
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateGeneReenableProb)
+	// mutate_add_node_prob 0.01
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateAddNodeProb)
+	// mutate_add_link_prob 0.3
+	fmt.Fscanf(r, "%s %f ", &name, &c.MutateAddLinkProb)
+	// interspecies_mate_rate 0.001
+	fmt.Fscanf(r, "%s %f ", &name, &c.InterspeciesMateRate)
+	// mate_multipoint_prob 0.6
+	fmt.Fscanf(r, "%s %f ", &name, &c.MateMultipointProb)
+	// mate_multipoint_avg_prob 0.4
+	fmt.Fscanf(r, "%s %f ", &name, &c.MateMultipointAvgProb)
+	// mate_singlepoint_prob 0.0
+	fmt.Fscanf(r, "%s %f ", &name, &c.MateSinglepointProb)
+	// mate_only_prob 0.2
+	fmt.Fscanf(r, "%s %f ", &name, &c.MateOnlyProb)
+	// recur_only_prob 0.2
+	fmt.Fscanf(r, "%s %f ", &name, &c.RecurOnlyProb)
+	// pop_size 1000
+	fmt.Fscanf(r, "%s %d ", &name, &c.PopSize)
+	// dropoff_age 15
+	fmt.Fscanf(r, "%s %d ", &name, &c.DropOffAge)
+	// newlink_tries 20
+	fmt.Fscanf(r, "%s %d ", &name, &c.NewLinkTries)
+	// print_every 60
+	fmt.Fscanf(r, "%s %d ", &name, &c.PrintEvery)
+	// babies_stolen 0
+	fmt.Fscanf(r, "%s %d ", &name, &c.BabiesStolen)
+	// num_runs 1
+	fmt.Fscanf(r, "%s %d ", &name, &c.NumRuns)
+
+	return &c
 }
 
 func (c *NeatContext) DebugLog(rec string) {
---
+++ neat/neat_test.go
@@ -0,0 +1,116 @@
+package neat
+
+import (
+	"testing"
+	"os"
+)
+
+func TestLoadContext(t *testing.T) {
+	config, err := os.Open("../data/p2nv.neat")
+	if err != nil {
+		t.Error("Failed to open config file", err)
+	}
+
+	// Load Neat Context
+	nc := LoadContext(config)
+	//t.Log(nc)
+	if nc.TraitParamMutProb != 0.5 {
+		t.Error("nc.TraitParamMutProb != 0.5")
+	}
+	if nc.TraitMutationPower != 1.0 {
+		t.Error("nc.TraitMutationPower != 1.0")
+	}
+	if nc.LinkTraitMutSig != 1.0 {
+		t.Error("nc.LinkTraitMutSig != 1.0")
+	}
+	if nc.NodeTraitMutSig != 0.5 {
+		t.Error("nc.NodeTraitMutSig != 0.5 ")
+	}
+	if nc.WeightMutPower != 1.8 {
+		t.Error("nc.WeightMutPower != 1.8")
+	}
+	if nc.recurProb != 0.05 {
+		t.Error("nc.recurProb != 0.05")
+	}
+	if nc.DisjointCoeff != 1.0 {
+		t.Error("nc.DisjointCoeff != 1.0")
+	}
+	if nc.ExcessCoeff != 1.0 {
+		t.Error("nc.ExcessCoeff != 1.0")
+	}
+	if nc.MutdiffCoeff != 3.0 {
+		t.Error("nc.MutdiffCoeff != 3.0")
+	}
+	if nc.CompatThreshold != 4.0 {
+		t.Error("CompatThreshold")
+	}
+	if nc.AgeSignificance != 1.0 {
+		t.Error("AgeSignificance")
+	}
+	if nc.SurvivalThresh != 0.4 {
+		t.Error("SurvivalThresh")
+	}
+	if nc.MutateOnlyProb != 0.25 {
+		t.Error("MutateOnlyProb")
+	}
+	if nc.MutateRandomTraitProb != 0.1 {
+		t.Error("MutateRandomTraitProb")
+	}
+	if nc.MutateLinkTraitProb != 0.1 {
+		t.Error("MutateLinkTraitProb")
+	}
+	if nc.MutateNodeTraitProb != 0.1 {
+		t.Error("MutateNodeTraitProb")
+	}
+	if nc.MutateLinkWeightsProb != 0.8 {
+		t.Error("MutateLinkWeightsProb")
+	}
+	if nc.MutateToggleEnableProb != 0.1 {
+		t.Error("MutateToggleEnableProb")
+	}
+	if nc.MutateGeneReenableProb != 0.05 {
+		t.Error("MutateGeneReenableProb")
+	}
+	if nc.MutateAddNodeProb != 0.01 {
+		t.Error("MutateAddNodeProb")
+	}
+	if nc.MutateAddLinkProb != 0.3 {
+		t.Error("MutateAddLinkProb")
+	}
+	if nc.InterspeciesMateRate != 0.001 {
+		t.Error("InterspeciesMateRate")
+	}
+	if nc.MateMultipointProb != 0.6 {
+		t.Error("MateMultipointProb")
+	}
+	if nc.MateMultipointAvgProb != 0.4 {
+		t.Error("MateMultipointAvgProb")
+	}
+	if nc.MateSinglepointProb != 0.0 {
+		t.Error("MateSinglepointProb")
+	}
+	if nc.MateOnlyProb != 0.2 {
+		t.Error("MateOnlyProb")
+	}
+	if nc.RecurOnlyProb != 0.2 {
+		t.Error("RecurOnlyProb")
+	}
+	if nc.PopSize != 1000 {
+		t.Error("PopSize")
+	}
+	if nc.DropOffAge != 15 {
+		t.Error("DropOffAge")
+	}
+	if nc.NewLinkTries != 20 {
+		t.Error("NewLinkTries")
+	}
+	if nc.PrintEvery != 60 {
+		t.Error("PrintEvery")
+	}
+	if nc.BabiesStolen != 0 {
+		t.Error("BabiesStolen")
+	}
+	if nc.NumRuns != 1 {
+		t.Error("NumRuns")
+	}
+}
