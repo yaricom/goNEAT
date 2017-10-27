42656ad6215bec1e68d79b6e3fe05e2e3ebde900
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-10-27 13:31:30 +0300

    Fixed Innovation and Mutator types to be package private and defined as separate types

--- neat/genetics/common.go
+++ neat/genetics/common.go
@@ -1,19 +1,24 @@
 // Package genetics holds data holders and helper utilities used to implement genetic evolution algorithm
 package genetics
 
-// The innovation method to be applied
+// The innovation method type to be applied
+type innovationType byte
+// Available innovation types
 const (
 	// The novelty will be introduced by new NN node
-	NEWNODE = iota
+	newNodeInnType innovationType = iota + 1
 	// The novelty will be introduced by new NN link
-	NEWLINK
+	newLinkInnType
 )
 
-// Mutators are variables that specify a kind of mutation of connection weights between NN nodes
+
+// The mutator type that specifies a kind of mutation of connection weights between NN nodes
+type mutatorType byte
+// Available mutator types
 const (
 	//This adds Gaussian noise to the weights
-	GAUSSIAN = iota
+	gaussianMutator mutatorType = iota + 1
 	//This sets weights to numbers chosen from a Gaussian distribution
-	COLD_GAUSSIAN
+	goldGaussianMutator
 )
 
