e7966c8f20325f92e80b013e74a1c6e64f17fde3
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-10-27 17:29:48 +0300

    Implemented GO friendly command line flags parsing

--- xor_runner.go
+++ xor_runner.go
@@ -8,61 +8,57 @@ import (
 	"github.com/yaricom/goNEAT/experiments"
 	"github.com/yaricom/goNEAT/neat"
 	"github.com/yaricom/goNEAT/neat/genetics"
+	"log"
+	"flag"
 )
 
 // The XOR experiment runner
 func main() {
-	out_dir_path, context_path, genome_path := "./out", "./data/xor.neat", "./data/xorstartgenes"
-	if len(os.Args) == 4 {
-		out_dir_path = os.Args[1]
-		context_path = os.Args[2]
-		genome_path = os.Args[3]
-	}
+	var out_dir_path = flag.String("out", "./out", "The output directory to store results")
+	var context_path = flag.String("context", "./data/xor.neat", "The execution context configuration file")
+	var genome_path = flag.String("genome", "./data/xorstartgenes", "The seed genome to start with")
+
+	flag.Parse()
 
 	// Seed the random-number generator with current time so that
 	// the numbers will be different every time we run.
 	rand.Seed(time.Now().Unix())
 
 	// Load context configuration
-	configFile, err := os.Open(context_path)
+	configFile, err := os.Open(*context_path)
 	if err != nil {
-		fmt.Println("Failed to load context")
-		return
+		log.Fatal("Failed to open context configuration file", err)
 	}
 	context := neat.LoadContext(configFile)
 
 	// Load Genome
 	fmt.Println("Loading start genome for XOR experiment")
-	genomeFile, err := os.Open(genome_path)
+	genomeFile, err := os.Open(*genome_path)
 	if err != nil {
 		fmt.Println("Failed to open genome file")
 		return
 	}
 	start_genome, err := genetics.ReadGenome(genomeFile, 1)
 	if err != nil {
-		fmt.Println("Failed to read start genome")
-		return
+		log.Fatal("Failed to read start genome", err)
 	}
 	fmt.Println(start_genome)
 
 	// Check if output dir exists
-	if _, err := os.Stat(out_dir_path); err == nil {
+	if _, err := os.Stat(*out_dir_path); err == nil {
 		// clear it
-		os.RemoveAll(out_dir_path)
+		os.RemoveAll(*out_dir_path)
 	}
 	// create output dir
-	err = os.MkdirAll(out_dir_path, os.ModePerm)
+	err = os.MkdirAll(*out_dir_path, os.ModePerm)
 	if err != nil {
-		fmt.Printf("Failed to create output directory, reason: %s", err)
-		return
+		log.Fatal("Failed to create output directory", err)
 	}
 
 	// The 100 generation XOR experiment
-	nodes, genes, evals, err := experiments.XOR(context, start_genome, out_dir_path)
+	nodes, genes, evals, err := experiments.XOR(context, start_genome, *out_dir_path)
 	if err != nil {
-		fmt.Println("Failed to perform XOR experiment:")
-		fmt.Println(err)
-		return
+		log.Fatal("Failed to perform XOR experiment!", err)
 	}
 
 	// Average and print stats
@@ -90,6 +86,6 @@ func main() {
 	fmt.Printf("\n>>>>>\nAverage Nodes:\t%d\nAverage Genes:\t%d\nAverage Evals:\t%d\n",
 		total_nodes / context.NumRuns, total_genes / context.NumRuns, total_evals / context.NumRuns)
 
-	fmt.Printf(">>> Start genome file:  %s\n", genome_path)
-	fmt.Printf(">>> Configuration file: %s\n", context_path)
+	fmt.Printf(">>> Start genome file:  %s\n", *genome_path)
+	fmt.Printf(">>> Configuration file: %s\n", *context_path)
 }
