0c6315e5b862784636c290da53d009f4a9a7d5d3
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2017-11-20 16:34:46 +0200

    Implemented double pole-balancing experiment

--- experiments/pole/cart2pole.go
+++ experiments/pole/cart2pole.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"github.com/yaricom/goNEAT/neat/genetics"
 	"os"
+	"sort"
 )
 
 const thirty_six_degrees = 36 * math.Pi / 180.0
@@ -58,7 +59,7 @@ func (ev *CartDoublePoleGenerationEvaluator) TrialRunStarted(trial *experiments.
 }
 
 // Perform evaluation of one epoch on double pole balancing
-func (ex *CartDoublePoleGenerationEvaluator) GenerationEvaluate(pop *genetics.Population, epoch *experiments.Generation, context *neat.NeatContext) (err error) {
+func (ex *CartDoublePoleGenerationEvaluator) GenerationEvaluate(pop *genetics.Population, generation *experiments.Generation, context *neat.NeatContext) (err error) {
 	ex.cartPole.nonMarkovLong = false
 	ex.cartPole.generalizationTest = false
 
@@ -68,27 +69,109 @@ func (ex *CartDoublePoleGenerationEvaluator) GenerationEvaluate(pop *genetics.Po
 
 		if res {
 			// This will be winner in Markov case
-			epoch.Solved = true
-			epoch.WinnerNodes = len(org.Genotype.Nodes)
-			epoch.WinnerGenes = org.Genotype.Extrons()
-			epoch.WinnerEvals = context.PopSize * epoch.Id + org.Genotype.Id
-			epoch.Best = org
+			generation.Solved = true
+			generation.WinnerNodes = len(org.Genotype.Nodes)
+			generation.WinnerGenes = org.Genotype.Extrons()
+			generation.WinnerEvals = context.PopSize * generation.Id + org.Genotype.Id
+			generation.Best = org
 			break // we have winner
 		}
 	}
 
 	// Check for winner in Non-Markov case
 	if !ex.Markov {
-		// TODO: finish implementation
+		// Sort the species by max organism fitness in descending order - the highest fitness first
+		sorted_species := make([]*genetics.Species, len(pop.Species))
+		copy(sorted_species, pop.Species)
+		sort.Sort(sort.Reverse(genetics.ByOrganismFitness(sorted_species)))
+
+		// First update what is checked and unchecked
+		var curr_species *genetics.Species
+		for _, curr_species = range sorted_species {
+			if max, _ := curr_species.ComputeMaxAndAvgFitness(); max > curr_species.MaxFitnessEver {
+				curr_species.IsChecked = false
+			}
+		}
+
+		// Now find first (most fit) species that is unchecked
+		curr_species = nil
+		for _, curr_species = range sorted_species {
+			if !curr_species.IsChecked {
+				break
+			}
+		}
+		if curr_species == nil {
+			curr_species = sorted_species[0]
+		}
+
+		// Remember it was checked
+		curr_species.IsChecked = true
+
+		// the organism champion
+		champion := curr_species.FindChampion()
+		champion_fitness := champion.Fitness
+
+		// Now check to make sure the champion can do 100000 evaluations
+		ex.cartPole.nonMarkovLong = true
+		ex.cartPole.generalizationTest = false
+
+		if ex.orgEvaluate(generation.Best) {
+			// the champion passed non-Markov long test
+			ex.cartPole.nonMarkovLong = false
+			// Given that the champ passed, now run it on generalization tests
+			state_vals := [5]float64{0.05, 0.25, 0.5, 0.75, 0.95}
+			score := 0
+			for s0c := 0; s0c <= 4; s0c++ {
+				for s1c := 0; s1c <= 4; s1c++ {
+					for s2c := 0; s2c <= 4; s2c++ {
+						for s3c := 0; s3c <= 4; s3c++ {
+							ex.cartPole.state[0] = state_vals[s0c] * 4.32 - 2.16
+							ex.cartPole.state[1] = state_vals[s1c] * 2.70 - 1.35
+							ex.cartPole.state[2] = state_vals[s2c] * 0.12566304 - 0.06283152 // 0.06283152 = 3.6 degrees
+							ex.cartPole.state[3] = state_vals[s3c] * 0.30019504 - 0.15009752 // 0.15009752 = 8.6 degrees
+							ex.cartPole.state[4] = 0.0
+							ex.cartPole.state[5] = 0.0
+
+							ex.cartPole.generalizationTest = true
+
+							// The champion needs to be flushed here because it may have
+							// leftover activation from its last test run that could affect
+							// its recurrent memory
+							generation.Best.Phenotype.Flush()
+
+							if ex.orgEvaluate(generation.Best) {
+								score++
+							}
+						}
+					}
+				}
+			}
+
+			if score >= 200 {
+				// The generalization test winner
+				neat.DebugLog(fmt.Sprintf("The non-Markov champion found! (generalization = %d)", score))
+				generation.Solved = true
+				generation.WinnerNodes = len(champion.Genotype.Nodes)
+				generation.WinnerGenes = champion.Genotype.Extrons()
+				generation.WinnerEvals = context.PopSize * generation.Id + champion.Genotype.Id
+				generation.Best = champion
+			} else {
+				neat.DebugLog("The non-Markov champion failed to generalize in non-Markov test")
+				generation.Best.Fitness = champion_fitness; // Restore the champ's fitness
+			}
+		} else {
+			neat.DebugLog("The non-Markov champion failed the 100,000 run in non-Markov test")
+			champion.Fitness = champion_fitness; // Restore the champ's fitness
+		}
 	}
 
 
 	// Fill statistics about current epoch
-	epoch.FillPopulationStatistics(pop)
+	generation.FillPopulationStatistics(pop)
 
 	// Only print to file every print_every generations
-	if epoch.Solved || epoch.Id % context.PrintEvery == 0 {
-		pop_path := fmt.Sprintf("%s/gen_%d", ex.OutputPath, epoch.Id)
+	if generation.Solved || generation.Id % context.PrintEvery == 0 {
+		pop_path := fmt.Sprintf("%s/gen_%d", ex.OutputPath, generation.Id)
 		file, err := os.Create(pop_path)
 		if err != nil {
 			neat.ErrorLog(fmt.Sprintf("Failed to dump population, reason: %s\n", err))
@@ -97,7 +180,7 @@ func (ex *CartDoublePoleGenerationEvaluator) GenerationEvaluate(pop *genetics.Po
 		}
 	}
 
-	if epoch.Solved {
+	if generation.Solved {
 		// print winner organism
 		for _, org := range pop.Organisms {
 			if org.IsWinner {
@@ -108,7 +191,7 @@ func (ex *CartDoublePoleGenerationEvaluator) GenerationEvaluate(pop *genetics.Po
 					neat.ErrorLog(fmt.Sprintf("Failed to dump winner organism genome, reason: %s\n", err))
 				} else {
 					org.Genotype.Write(file)
-					neat.InfoLog(fmt.Sprintf("Generation #%d winner dumped to: %s\n", epoch.Id, org_path))
+					neat.InfoLog(fmt.Sprintf("Generation #%d winner dumped to: %s\n", generation.Id, org_path))
 				}
 				break
 			}
@@ -116,7 +199,7 @@ func (ex *CartDoublePoleGenerationEvaluator) GenerationEvaluate(pop *genetics.Po
 	} else {
 		// Move to the next epoch if failed to find winner
 		neat.DebugLog(">>>>> start next generation")
-		_, err = pop.Epoch(epoch.Id + 1, context)
+		_, err = pop.Epoch(generation.Id + 1, context)
 	}
 
 	return err
@@ -170,8 +253,6 @@ func (cp *CartPole)evalNet(net *network.Network) (steps float64) {
 	non_markov_max := 1000.0
 	if cp.nonMarkovLong {
 		non_markov_max = 100000.0
-	} else if cp.generalizationTest {
-		non_markov_max = 1000.0
 	}
 
 	input := make([]float64, 7)
