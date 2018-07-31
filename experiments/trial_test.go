ec0cfffa53de44ea2d8e350564e23f349f02b9f1
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-07-31 18:59:00 +0300

    Implemented test Trial objects factory

--- experiments/trial_test.go
+++ experiments/trial_test.go
@@ -8,10 +8,7 @@ import (
 )
 
 func TestTrial_Encode_Decode(t *testing.T) {
-	trial := Trial{Id:1, Generations:make([]Generation, 3)}
-	for i := 0; i < len(trial.Generations); i++ {
-		trial.Generations[i] = *buildTestGeneration(i + 1, float64(i + 1) * math.E)
-	}
+	trial := buildTestTrial(1, 3)
 
 	var buff bytes.Buffer
 	enc := gob.NewEncoder(&buff)
@@ -35,7 +32,7 @@ func TestTrial_Encode_Decode(t *testing.T) {
 	}
 
 	// do deep compare of Trail fields
-	deepCompareTrials(&trial, &dec_trial, t)
+	deepCompareTrials(trial, &dec_trial, t)
 }
 
 func deepCompareTrials(first, second *Trial, t *testing.T) {
@@ -50,4 +47,12 @@ func deepCompareTrials(first, second *Trial, t *testing.T) {
 	for i := 0; i < len(first.Generations); i++ {
 		deepCompareGenerations(&first.Generations[i], &second.Generations[i], t)
 	}
+}
+
+func buildTestTrial(id, num_generations int) *Trial {
+	trial := Trial{Id:id, Generations:make([]Generation, num_generations)}
+	for i := 0; i < num_generations; i++ {
+		trial.Generations[i] = *buildTestGeneration(i + 1, float64(i + 1) * math.E)
+	}
+	return &trial
 }
