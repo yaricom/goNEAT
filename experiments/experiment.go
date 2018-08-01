8f90f2ac5a3558ff2b43bfef4ba9781bd6837c05
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-08-01 19:15:50 +0300

    Implemented function to gather average statistics per trial in the experiment. Fixed naming for best organism statistics and amended best organism search function to include 'onlySolvers' parameter. Implemented print total experiment statistics function.

--- experiments/experiment.go
+++ experiments/experiment.go
@@ -6,12 +6,13 @@ import (
 	"github.com/yaricom/goNEAT/neat/genetics"
 	"io"
 	"encoding/gob"
+	"fmt"
 )
 
 // An Experiment is a collection of trials for one experiment. It's useful for statistical analysis of a series of
 // experiments
 type Experiment struct {
-	Id          int
+	Id   int
 	Name string
 	Trials
 }
@@ -27,13 +28,26 @@ func (e Experiment) LastExecuted() time.Time {
 	return u
 }
 
-func (e Experiment) Best() *genetics.Organism {
-	var orgs = make(genetics.Organisms, len(e.Trials))
+// Finds the most fit organism among all epochs in this trial. It's also possible to get the best organism only among the ones
+// which was able to solve the experiment's problem. Returns the best fit organism in this experiment among with ID of trial
+// where it was found and boolean value to indicate if search was successful.
+func (e Experiment) BestOrganism(onlySolvers bool) (*genetics.Organism, int, bool) {
+	var orgs = make(genetics.Organisms, 0, len(e.Trials))
 	for i, t := range e.Trials {
-		orgs[i] = t.Best()
+		org, found := t.BestOrganism(onlySolvers)
+		if found {
+			orgs = append(orgs, org)
+			org.Flag = i
+		}
+
 	}
-	sort.Sort(sort.Reverse(orgs))
-	return orgs[0]
+	if len(orgs) > 0 {
+		sort.Sort(sort.Reverse(orgs))
+		return orgs[0], orgs[0].Flag, true
+	} else {
+		return nil, -1, false
+	}
+
 }
 
 func (e Experiment) Solved() bool {
@@ -45,29 +59,35 @@ func (e Experiment) Solved() bool {
 	return false
 }
 
-// Fitness returns the fitnesses of the best genome of each trial
-func (e Experiment) Fitness() Floats {
+// The fitness values of the best organisms for each trial
+func (e Experiment) BestFitness() Floats {
 	var x Floats = make([]float64, len(e.Trials))
 	for i, t := range e.Trials {
-		x[i] = t.Best().Fitness
+		if org, ok := t.BestOrganism(false); ok {
+			x[i] = org.Fitness
+		}
 	}
 	return x
 }
 
-// Novelty returns the age values of the best species of each trial
-func (e Experiment) Age() Floats {
+// The age values of the organisms for each trial
+func (e Experiment) BestAge() Floats {
 	var x Floats = make([]float64, len(e.Trials))
 	for i, t := range e.Trials {
-		x[i] = float64(t.Best().Species.Age)
+		if org, ok := t.BestOrganism(false); ok {
+			x[i] = float64(org.Species.Age)
+		}
 	}
 	return x
 }
 
-// Complexity returns the complexity of the best genome of each trial
-func (e Experiment) Complexity() Floats {
+// The complexity values of the best organisms for each trial
+func (e Experiment) BestComplexity() Floats {
 	var x Floats = make([]float64, len(e.Trials))
 	for i, t := range e.Trials {
-		x[i] = float64(t.Best().Phenotype.Complexity())
+		if org, ok := t.BestOrganism(false); ok {
+			x[i] = float64(org.Phenotype.Complexity())
+		}
 	}
 	return x
 }
@@ -90,17 +110,18 @@ func (e Experiment) Epochs() Floats {
 	return x
 }
 
-// Returns average number of nodes, genes and organisms evaluations of winner genomes among all trials, i.e. for all trials
-// where winning solution was found
-func (e Experiment) AvgWinnerNGE() (avg_nodes, avg_genes, avg_evals float64) {
-	total_nodes, total_genes, total_evals := 0, 0, 0
+// Returns average number of nodes, genes, organisms evaluations, and species diversity of winner genomes among all
+// trials, i.e. for all trials where winning solution was found
+func (e Experiment) AvgWinner() (avg_nodes, avg_genes, avg_evals, avg_diversity float64) {
+	total_nodes, total_genes, total_evals, total_diversity := 0, 0, 0, 0
 	count := 0
 	for _, t := range e.Trials {
 		if t.Solved() {
-			nodes, genes, evals := t.WinnerNGE()
+			nodes, genes, evals, diversity := t.Winner()
 			total_nodes += nodes
 			total_genes += genes
 			total_evals += evals
+			total_diversity += diversity
 
 			count++
 		}
@@ -108,17 +129,54 @@ func (e Experiment) AvgWinnerNGE() (avg_nodes, avg_genes, avg_evals float64) {
 	avg_nodes = float64(total_nodes) / float64(count)
 	avg_genes = float64(total_genes) / float64(count)
 	avg_evals = float64(total_evals) / float64(count)
-	return avg_nodes, avg_genes, avg_evals
+	avg_diversity = float64(total_diversity) / float64(count)
+	return avg_nodes, avg_genes, avg_evals, avg_diversity
+}
+
+// Prints experiment statistics
+func (ex Experiment) PrintStatistics() {
+	// Print absolute champion statistics
+	if org, trid, found := ex.BestOrganism(true); found {
+		nodes, genes, evals, divers := ex.Trials[trid].Winner()
+		fmt.Printf("\nChampion found in %d trial\n\tWinner Nodes:\t%.1f\n\tWinner Genes:\t%.1f\n\tWinner Evals:\t%.1f\n\tDiversity:\t%.1f\n",
+			trid, nodes, genes, evals, divers)
+		fmt.Printf("\tComplexity:\t%d\n\tAge:\t\t%d\n",
+			org.Phenotype.Complexity(), org.Species.Age)
+	} else {
+		fmt.Println("\nNo winner found in the experiment!!!")
+	}
+
+	// Print average winner statistics
+	if len(ex.Trials) > 1 {
+		avg_nodes, avg_genes, avg_evals, avg_divers := ex.AvgWinner()
+
+		fmt.Printf("\nAverage\n\tWinner Nodes:\t%.1f\n\tWinner Genes:\t%.1f\n\tWinner Evals:\t%.1f\n\tDiversity:\t%.1f\n",
+			avg_nodes, avg_genes, avg_evals, avg_divers)
+	}
+
+	// Print best organisms statistics per epoch per trial, i.e. every the best found
+	mean_complexity, mean_diversity, mean_age := 0.0, 0.0, 0.0
+	for _, t := range ex.Trials {
+		mean_complexity += t.BestComplexity().Mean()
+		mean_diversity += t.Diversity().Mean()
+		mean_age += t.BestAge().Mean()
+	}
+	count := float64(len(ex.Trials))
+	mean_complexity /= count
+	mean_diversity /= count
+	mean_age /= count
+	fmt.Printf("Mean of the all most fit organisms found\n\tComplexity:\t%.1f\n\tDiversity:\t%.1f\n\tAge:\t\t%.1f\n", mean_complexity, mean_diversity, mean_age)
+
 }
 
 // Encodes experiment and writes to provided writer
-func (ex *Experiment) Write(w io.Writer) error {
+func (ex Experiment) Write(w io.Writer) error {
 	enc := gob.NewEncoder(w)
 	return ex.Encode(enc)
 }
 
 // Encodes experiment with GOB encoding
-func (ex *Experiment) Encode(enc *gob.Encoder) error {
+func (ex Experiment) Encode(enc *gob.Encoder) error {
 	err := enc.Encode(ex.Id)
 	err = enc.Encode(ex.Name)
 
@@ -152,7 +210,7 @@ func (ex *Experiment) Decode(dec *gob.Decoder) error {
 	}
 
 	ex.Trials = make([]Trial, t_num)
-	for i := 0; i< t_num; i++ {
+	for i := 0; i < t_num; i++ {
 		trial := Trial{}
 		err = trial.Decode(dec)
 		ex.Trials[i] = trial
@@ -163,8 +221,12 @@ func (ex *Experiment) Decode(dec *gob.Decoder) error {
 // Experiments is a sortable list of experiments by execution time and Id
 type Experiments []Experiment
 
-func (es Experiments) Len() int      { return len(es) }
-func (es Experiments) Swap(i, j int) { es[i], es[j] = es[j], es[i] }
+func (es Experiments) Len() int {
+	return len(es)
+}
+func (es Experiments) Swap(i, j int) {
+	es[i], es[j] = es[j], es[i]
+}
 func (es Experiments) Less(i, j int) bool {
 	ui := es[i].LastExecuted()
 	uj := es[j].LastExecuted()
