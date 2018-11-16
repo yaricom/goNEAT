a4c965b49d9bf0cd2cfec8558c56426c27b4a088
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-11-16 16:22:51 +0200

    Added genome encoding types definitions and common errors during to raise during genome reading

--- neat/genetics/common.go
+++ neat/genetics/common.go
@@ -1,6 +1,8 @@
 // Package genetics holds data holders and helper utilities used to implement genetic evolution algorithm
 package genetics
 
+import "errors"
+
 // The innovation method type to be applied
 type innovationType byte
 // Available innovation types
@@ -20,5 +22,19 @@ const (
 	gaussianMutator mutatorType = iota + 1
 	//This sets weights to numbers chosen from a Gaussian distribution
 	goldGaussianMutator
+)
+
+// Defines format of Genome data encoding
+type GenomeEncoding byte
+
+const (
+	// The plain text
+	PlainGenomeEncoding GenomeEncoding = iota + 1
+	// The rich text in YAML
+	YAMLGenomeEncoding
+)
+
+var (
+	ErrUnsupportedGenomeEncoding = errors.New("unsupported genome encoding")
 )
 
