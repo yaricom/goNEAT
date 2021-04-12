a4affd3dde2b444eaba977187a1ae3275146b348
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2021-04-12 19:02:42 +0300

    Fixed Trait documentation.

--- neat/trait.go
+++ neat/trait.go
@@ -7,15 +7,14 @@ import (
 	"math/rand"
 )
 
-// The number of parameters used in neurons that learn through habituation,
-// sensitization, or Hebbian-type processes
+// NumTraitParams The number of parameters used in neurons that learn through habituation, sensitization or Hebbian-type processes
 const NumTraitParams = 8
 
 var (
 	ErrTraitsParametersCountMismatch = errors.New("traits parameters number mismatch")
 )
 
-// TRAIT: A Trait is a group of parameters that can be expressed as a group more than one time. Traits save a genetic
+// Trait is a group of parameters that can be expressed as a group more than one time. Traits save a genetic
 // algorithm from having to search vast parameter landscapes on every node. Instead, each node can simply point to a trait
 // and those traits can evolve on their own.
 type Trait struct {
@@ -25,12 +24,13 @@ type Trait struct {
 	Params []float64
 }
 
+// NewTrait is to create empty trait with default parameters number (see: NumTraitParams above)
 func NewTrait() *Trait {
 	trait := newTrait(NumTraitParams)
 	return trait
 }
 
-// The copy constructor
+// NewTraitCopy The copy constructor
 func NewTraitCopy(t *Trait) *Trait {
 	nt := newTrait(len(t.Params))
 	nt.Id = t.Id
@@ -40,7 +40,7 @@ func NewTraitCopy(t *Trait) *Trait {
 	return nt
 }
 
-// Special Constructor creates a new Trait which is the average of two existing traits passed in
+// NewTraitAvrg Special Constructor creates a new Trait which is the average of two existing traits passed in
 func NewTraitAvrg(t1, t2 *Trait) (*Trait, error) {
 	if len(t1.Params) != len(t2.Params) {
 		return nil, ErrTraitsParametersCountMismatch
@@ -60,7 +60,7 @@ func newTrait(length int) *Trait {
 	}
 }
 
-// Perturb the trait parameters slightly
+// Mutate perturb the trait parameters slightly
 func (t *Trait) Mutate(traitMutationPower, traitParamMutProb float64) {
 	for i := 0; i < len(t.Params); i++ {
 		if rand.Float64() > traitParamMutProb {
